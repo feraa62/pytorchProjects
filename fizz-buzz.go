832c541cc03d99b9e308ac2de40ea50fada66d70
feraa62 <feraa@tutamail.com>
2024-07-15 23:57:04 +0300

    Update config files

---
+++ fizz-buzz.go
@@ -0,0 +1,5 @@
+// 186 chars
+
+package main
+import x"fmt"
+func main(){for i:=1; i<101; i++{if i%3==0&&i%5==0{x.Println("FizzBuzz")}else if i%3==0{x.Println("Fizz")}else if i%5==0{x.Println("Buzz")}else{x.Println(i)}}}
